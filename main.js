a7f38ac8d4943da4cd14a9e0baedf4544b73ae80
Sr. Ganso <103332962+CodeGanso@users.noreply.github.com>
2022-04-29 16:07:11 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+var arrayImages = ["img", "img", "img", "img", "img"];
+
+
+var arrayNombres = ["name", "mame", "name", "name", "name"];
+
+
+var arrayVersiones = ["1.12.3", "img", "img", "img", "img"];
+
+
+var arrayDesarrolladores = ["name", "mame", "name", "name", "name"];
+
+var clave = "";
+
+
+
+var numberRandom = aleatorio(1, 9);
+
+var mmm = numberRandom*8755;
+var ddd = (mmm/2)*1.5;
+var mmmm = parseInt(ddd*2.5);
+
+clave = ""+mmmm+numberRandom;
+
+
+enviar();
+function enviar() {
+  AndroidInterface.obtenerDatos(arrayImages, arrayNombres, arrayVersiones, arrayDesarrolladores, clave);
+}
+
+
+
+
+//Generar números aleatorios
+function aleatorio(inferior, superior) {
+  var numPosibilidades = superior - inferior;
+  var aleatorio = Math.random() * (numPosibilidades + 1);
+  aleatorio = Math.floor(aleatorio);
+  return inferior + aleatorio;
+} 
+
